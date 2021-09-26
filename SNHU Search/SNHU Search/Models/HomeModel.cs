7d26073f56bb20175fe14bb712b7b3e355c5d25d
Aaliyah Ortiz <60981280+aaliyahortiz26@users.noreply.github.com>
2021-09-26 15:31:22 -0400

    adding home model

---
+++ SNHU Search/SNHU Search/Models/HomeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.ComponentModel.DataAnnotations;
+
+namespace SNHU_Search.Models
+{
+    public class HomeModel
+    {
+        public string keywordSearch { get; set; }
+    }
+}
