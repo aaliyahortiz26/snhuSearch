6a224017deeab4c6d808c65d2820a4fcb550cb69
Noah McKearney <noah.mckearney@snhu.edu>
2021-09-26 21:05:15 -0400

    added DBManager to connect to database

---
+++ SNHU Search/SNHU Search/Models/DBManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Linq;
+using System.IO;
+using System.Threading;
+
+namespace SNHU_Search.Models
+{
+    public class DBManager
+    {
+        public string ConnectionString { get; set; }
+
+        public DBManager(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+        private MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+
+        #region Login/Create Account
+
+
+        #endregion
+    }
+}
