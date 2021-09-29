1108641d10047a0207a8c66732143568eb480215
Aaliyah Ortiz <60981280+aaliyahortiz26@users.noreply.github.com>
2021-09-29 18:38:08 -0400

    added the changes from the main branch (github was acting weird for me so I went through and added them in myself)

---
+++ SNHU Search/SNHU Search/Models/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SNHU_Search.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Enter Your UserName. *")]
+        [Display(Name = "UserName")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Enter Your Password. *")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Pass")]
+        public string Password { get; set; }
+    }
+}
