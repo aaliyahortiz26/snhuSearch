ad7e5bfe087f1131cfb2b25fbad9b319cc538767
Noah McKearney <noah.mckearney@snhu.edu>
2021-10-29 10:34:54 -0400

    added elasticsearch class

---
+++ SNHU Search/SNHU Search/Models/ElasticManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Nest;
+
+namespace SNHU_Search.Models
+{
+    public class ElasticManager
+    {
+        private readonly ElasticClient _client;
+
+
+        public void addData()
+        {
+            ISearchResponse<website> results;
+            
+        }
+    }
+}
