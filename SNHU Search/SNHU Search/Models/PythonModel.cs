eccde98916af67b505eea7910b4850f47c37e159
jesse.thomas@snhu.edu <56457691+jessethomascs@users.noreply.github.com>
2021-11-27 20:50:46 -0500

    Started implementation of C# to Python
    Runs python file as is with command prompt

---
+++ SNHU Search/SNHU Search/Models/PythonModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+
+
+namespace SNHU_Search.Models
+{
+    public class PythonModel
+    {
+
+        public void Scrape(/*string cmd, string args*/)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C python WebScrape.py";
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+    }
+}
