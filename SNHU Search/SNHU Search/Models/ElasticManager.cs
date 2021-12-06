97ca7af0c6257d8fef128171ceed1440d08858c0
NoahMcKearney <60981324+NoahMcKearney@users.noreply.github.com>
2021-12-06 07:13:17 -0500

    Updated connection string

--- SNHU Search/SNHU Search/Models/ElasticManager.cs
+++ SNHU Search/SNHU Search/Models/ElasticManager.cs
@@ -10,7 +10,7 @@ namespace SNHU_Search.Models
 {
     public class ElasticManager
     {
-        private string elasticConnection = "http://3.82.206.132:9200/";
+        private string elasticConnection = "http://3.84.22.239:9200/";
         public struct WebsiteDetails
         {
             public string Keywords;
